8c03a00b10c27e7442c7d9e44377fb5b120a8613
CrusxPerf <santoshkmpatra@gmail.com>
2025-06-18 03:12:23 +0530

    Delete AIPerformanceFrameowork/src directory

--- AIPerformanceFrameowork/src/__init__.py
+++
@@ -1,3 +0,0 @@
-# This __init__.py file can be left blank.
-# Its presence makes 'src' a Python package, allowing imports like 'from src.some_module import ...'.
-# You can also add package-level initializations here if needed in the future.
